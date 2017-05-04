c716ab7cb47a176c75a814d5122851e78d8497f4
junjchen <chuckjunjchen@gmail.com>
2017-05-04 21:24:56 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,77 @@
+function queryNutriApi(query) {
+    var nutriURL = 'https://trackapi.nutritionix.com/v2/natural/nutrients';
+
+    return fetch(nutriURL, {
+        method: 'POST',
+        redirect: 'follow',
+        headers: new Headers({
+            'Content-Type': 'application/JSON',
+            'x-app-id': '4b62710c',
+            'x-app-key': '6fa130a6fe641daca440813fa94c98d0'
+        }),
+        body: JSON.stringify({
+            'query': query,
+            'timezone': 'US/Eastern'
+        })
+    }).
+    then(function (res) {
+        return res.json();
+    }).
+    then(function (data) {
+        return data.foods.reduce(function (previous, current) {
+            var name = current.food_name;
+            if (!previous[name]) {
+                previous[name] = {
+                    name: name,
+                    weight: current.serving_weight_grams,
+                    calories: current.nf_calories
+                }
+            } else {
+                previous[name].weight += current.serving_weight_grams;
+                previous[name].calories += current.nf_calories;
+            }
+            return previous;
+        }, []);
+    });
+}
+
+function getAppData() {
+    var demoData = [{
+        name: 'Vivian Home',
+        foods: [{
+                name: 'Pan fried egg',
+                recipe: '1 large egg, 1 tsp salt, 5g olive oil'
+            },
+            {
+                name: 'Honey milk',
+                recipe: '0.3 liter milk, 1 tsp honey'
+            }
+        ]
+    }];
+
+    var getFoodCalories = function (food) {
+        return queryNutriApi(food.recipe).then(function (n) {
+            food.nutritions = n;
+            return food;
+        })
+    }
+
+    var getResFoodCalories = function (resaurants) {
+        return [].concat.apply([], restaurants.map(function (r) {
+            return r.foods.map(function (f) {
+                return getFoodCalories(f);
+            });
+        }));
+    }
+
+    Promise.all(getFoodCalories(demoData)).then(function (result) {
+        console.log(result);
+    })
+}
+
+// var q = '1/2 cup vegetable oil, 6 anchovy fillets ,1 small garlic clove , 2 large eggs ,2 tablespoons lemon juice , 3/4 teaspoon Dijon mustard , 2 tablespoon olive oil , 3 tablespoons ground black pepper , 1 pieces bread , 3 tablespoons olive oil , 3 romaine hearts';
+// queryNutriApi(q).then(function (result) {
+//     console.log(result);
+//});
+
+getAppData();
