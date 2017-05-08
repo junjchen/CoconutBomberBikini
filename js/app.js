6ceac353bda4938003606d23561b07e9dde8aa29
yukisong192517 <626606901@qq.com>
2017-05-08 16:28:04 +0100

    loadData works, return the who restaurant objects

--- js/app.js
+++ js/app.js
@@ -14,55 +14,53 @@ function queryNutriApi(query) {
             'timezone': 'US/Eastern'
         })
     }).
-    then(function (res) {
-        return res.json();
-    }).
-    then(function (data) {
-        console.log(data.food);
-        return data.foods.reduce(function (previous, current) {
-            var name = current.food_name;
-            if (!previous[name]) {
-                previous[name] = {
-                    name: name,
-                    weight: current.serving_weight_grams,
-                    calories: current.nf_calories
+        then(function (res) {
+            return res.json();
+        }).
+        then(function (data) {
+            return data.foods.reduce(function (previous, current) {
+                var name = current.food_name;
+                if (!previous[name]) {
+                    previous[name] = {
+                        name: name,
+                        weight: current.serving_weight_grams,
+                        calories: current.nf_calories
+                    }
+                } else {
+                    previous[name].weight += current.serving_weight_grams;
+                    previous[name].calories += current.nf_calories;
                 }
-            } else {
-                previous[name].weight += current.serving_weight_grams;
-                previous[name].calories += current.nf_calories;
-            }
-            return previous;
-        }, []);
-    });
+                return previous;
+            }, []);
+        });
 }
 
-var loadData = (function () {
+var loadData = function () {
+    var retval;
     return fetch('demo.json').then(function (res) {
-    return res.json();
+        return res.json();
     }).then(function (data) {
-    //data is the object dataset of restaurant and foods;
-     for (var key in data) 
-     {
-         //food is all kinds of food in certain restaurant
-        var food = data[key].foods;
-        food.map(function(f){
-            //for each recipe in food ,ask for nutrition and add ingredient calories and total calories in recipe;
-            //return recipe
-            return queryNutriApi(f.recipe).then(function (n) 
-             {
-                f.nutritions = n;
-                f.totalCalories =  Object.keys(n).reduce(function(previous, current){
-                    if(current)
-                    {
-                        previous += n[current].calories;
-                    }
-                    return previous;},0);
-                //?????return f;no return, less time
-                //not sure should return 'f' or not 
-             });
-        }); 
-    };
-    //return data;
-    console.log(data);
-})
-});
+        retval = data;
+        //data is the object dataset of restaurant and foods;
+        var promises = [];
+        for (var key in data) {
+            //food is all kinds of food in certain restaurant
+            var food = data[key].foods;
+            var newFood = food.map(function (f) {
+                //for each recipe in food ,ask for nutrition and add ingredient calories and total calories in recipe;
+                //return recipe
+                return queryNutriApi(f.recipe).then(function (n) {
+                    f.nutritions = n;
+                    f.totalCalories = Object.keys(n).reduce(function (previous, current) {
+                        if (current) {
+                            previous += n[current].calories;
+                        }
+                        return previous;
+                    }, 0);
+                });
+            });
+            promises.push(Promise.all(newFood));
+        };
+        return promises;
+    }).then(function () { return retval; })
+};
